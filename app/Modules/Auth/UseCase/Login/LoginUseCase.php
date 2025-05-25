97ff7b996f7d5ffb37674025cd5e5e4a8708ecac
Jhonatan Henkel <96666648+Jhon-Henkel@users.noreply.github.com>
2025-05-25 18:23:51 -0300

    Update LoginUseCase.php

--- app/Modules/Auth/UseCase/Login/LoginUseCase.php
+++ app/Modules/Auth/UseCase/Login/LoginUseCase.php
@@ -2,20 +2,26 @@
 
 namespace App\Modules\Auth\UseCase\Login;
 
+use App\Infra\Response\Exceptions\BadRequestException;
 use App\Models\User\User;
 
 class LoginUseCase
 {
-    public function execute(string $login, string $password): string
+    public function execute(string $login, string $password): array
     {
         $user = User::where('email', $login)->first();
-        if (! $this->isValidLogin($user, $password)) {
-            return 'invalid_credentials';
+        if (!$this->isValidLogin($user, $password)) {
+            throw new BadRequestException('Credenciais inválidas');
         }
         $user->tokens()->delete();
         $token = $user->createToken("{$user->email}_token")->plainTextToken;
         auth()->login($user);
-        return $token;
+        return [
+            'token' => $token,
+            'name' => $user->name,
+            'email' => $user->email,
+            'id' => $user->id,
+        ];
     }
 
     protected function isValidLogin(User|null $user, string $password): bool
@@ -23,7 +29,7 @@ class LoginUseCase
         if (empty($user)) {
             return false;
         }
-        if (! password_verify($password, $user->password)) {
+        if (!password_verify($password, $user->password)) {
             return false;
         }
         return true;
